ec3f5685e4afffdb83e13685bba5132dc868e501
sushuangbao <316049938@qq.com>
2024-03-10 22:00:23 +0800

    力扣

--- afterclass/searchInsertPosition/main.go
+++ afterclass/searchInsertPosition/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+/*
+35. 搜索插入位置：
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+请必须使用时间复杂度为 O(log n) 的算法。
+*/
 func main() {
 	res1 := searchInsert([]int{1, 3, 5, 6}, 5)
 	fmt.Printf("%v \n", res1)
